9b6e939d3f58ae6acd507a88202d7822258244fb
Ricardo Pereira - Veracode <20283870+cadonuno@users.noreply.github.com>
2024-09-04 12:13:05 +0100

    switching to new Grid tool

--- scripts-1.6.js
+++ scripts-1.6.js
@@ -1,39 +1,6 @@
 const GRID_ID = "gridjs";
 const DATABASE_FILE = "/veracode-tags-test/database.txt";
 
-var items = [];
-var grid = null;
-
-function searchSelection(cell, rowIndex, cellIndex) {
-    var searchValue = $("input").val()
-    if (searchValue == null || searchForValue == "") {
-        return "";
-    }
-    searchValue = searchValue.trim().toLowerCase()
-    if (searchValue.startsWith("name=")) {
-        return cellIndex == 0 ? searchForValue(cell, searchValue) : "";
-    } else if (searchValue.startsWith("url=")) {
-        return cellIndex == 1 ? searchForValue(cell, searchValue) : "";
-    } else if (searchValue.startsWith("description=")) {
-        return cellIndex == 2 ? searchForValue(cell, searchValue) : "";
-    } else if (searchValue.startsWith("author=")) {
-        return cellIndex == 3 ? searchForValue(cell, searchValue) : "";
-    } else if (searchValue.startsWith("tags=")) {
-        return cellIndex == 5 ? searchForValue(cell, searchValue) : "";
-    }
-    return cell == null ? "" : cell;
-}
-
-function triggerTagSearch(value) {
-    var inputField = document.querySelector('.gridjs-search-input');
-    inputField.value = "tags=" + value;
-
-    inputField.dispatchEvent(new Event('input', {
-        bubbles: true,
-        cancelable: true,
-    }));
-}
-
 function searchForValue(cell, searchValue) {
     if (cell == null) {
         return "";
@@ -97,89 +64,60 @@ function escapeHTML(str) {
         });
 }
 
-function reorderGrid(orderColumn) {
-    items.sort((a,b) => (a[orderColumn] > b[orderColumn]) ? 1 : ((b[orderColumn] > a[orderColumn]) ? -1 : 0));
-    renderGrid(GRID_ID);
+async function triggerSearch(field, value) {
+    await api.setColumnFilterModel(field, {
+        filterType: 'customFilter',
+        type: 'contains',
+        filter: value,
+     });
+
+     api.onFilterChanged();
 }
 
-function renderGrid() {
-    grid.render(document.getElementById(GRID_ID))
+async function triggerTagSearch(value) {
+    triggerSearch("tags", value);
+}
+
+function buildTagsHtml(tagsField) {
+    elements = (tagsField == null) ? [] : tagsField.split(",");
+    html = '';
+    elements.forEach((element) => {
+        var trimmed = element.trim();
+        if (html) {
+            html += ', ';
+        }
+        html += "<a href=\"#\" onclick=\"triggerTagSearch('" + escapeHTML(trimmed) + "')\">" + escapeHTML(trimmed) + "</a>";
+    });
 }
 
 function populateGrid() {
+    var items = []
+    
     $.get(DATABASE_FILE, function(data) {
         items = data.split("\n");
         items.shift();
         while (items[items.length-1] === '') {
             items.pop();
         }
-        items = items.map(function(element) { 
+        gridData = items.map(function(element) { 
             var newLine = element.split("\t");
-            return newLine;
-        });			
-        items.sort((a,b) => (a[0] > b[0]) ? 1 : ((b[0] > a[0]) ? -1 : 0))
-        grid = new gridjs.Grid({
-            search: {
-                ignoreHiddenColumns: false,
-                selector: (cell, rowIndex, cellIndex) => searchSelection(cell, rowIndex, cellIndex)
-            },
-            sort: true,
-            pagination: true,
-            columns: [{ 
-                name: 'Name',
-                formatter: (_, row) => gridjs.html(`${buildLinkHtml(row.cells[4].data, row.cells[0].data)}`)
-            },
-            { 
-                name: 'Description',
-                formatter: (_, row) => gridjs.html(`${buildDescriptionHtml(row)}`)
-            },
-            { 
-                name: 'Author',
-                formatter: (_, row) => gridjs.html(`${buildLinkHtml(row.cells[5].data, row.cells[2].data)}`)
-            },
-            { 
-                name: 'Tags',
-                formatter: (_, row) => {
-                    elements = (row == null || row.cells[5] == null || row.cells[3].data == null) ? [] : row.cells[3].data.split(",");
-                    html = '';
-                    elements.forEach((element) => {
-                        var trimmed = element.trim();
-                        if (html) {
-                            html += ', ';
-                        }
-                        html += "<a href=\"#\" onclick=\"triggerTagSearch('" + escapeHTML(trimmed) + "')\">" + escapeHTML(trimmed) + "</a>";
-                    });
-                    return gridjs.html(`${html}`);
-                }								
-            },
-            {
-                name: 'repo-name',
-                hidden: true
-            },
-            {
-                name: 'repo-url',
-                hidden: true
-            },
-            {
-                name: 'repo-description',
-                hidden: true
-            },
-            {
-                name: 'author-name',
-                hidden: true
-            },
-            {
-                name: 'author-url',
-                hidden: true
-            },
-            {
-                name: 'repo-tags',
-                hidden: true
-            }],			        
-            resizable: true,
-            data: items
+            return {
+                "name": buildLinkHtml(newLine[4].data, newLine[0].data),
+                "description": buildDescriptionHtml(newLine),
+                "author": buildLinkHtml(newLine[5].data, newLine[2].data),
+                "tags": buildTagsHtml(newLine[3]),
+            };
         });
-        renderGrid()
+        var gridOptions = {
+            columnDefs: [
+                {  headerName: "Name", field: "name", filter: true, flex: 2 },
+                {  headerName: "Description", field: "description", filter: true, flex: 3 },
+                {  headerName: "Author", field: "author", filter: true, flex: 1 },
+                {  headerName: "Tags", field: "tags", filter: true, flex: 1 }
+            ],
+            rowData: gridData
+        }
+        agGrid.createGrid(document.getElementById(GRID_ID), gridOptions)
     });
     
 }
